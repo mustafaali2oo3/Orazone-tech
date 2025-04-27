db81a28c557d30377076f3d83cb55e65fc106ee3
Mustafa Ali <157490542+mustafaali2oo3@users.noreply.github.com>
2025-04-27 17:41:25 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -70,8 +70,8 @@ export default function Home() {
               <Image
                 src="/Logo.png" // Path to your logo in public folder
                 alt="Company Logo" // Descriptive alt text
-                width={110} // Adjust to your logo's width
-                height={20} // Adjust to your logo's height
+                width={70} // Adjust to your logo's width
+                height={10} // Adjust to your logo's height
                 className="h-auto object-contain" // Maintain aspect ratio
                 priority // Important for above-the-fold logo
               />
