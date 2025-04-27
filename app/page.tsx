d0a16ccc032adc3d0e2ff1c5b1037ed635436eec
Mustafa Ali <157490542+mustafaali2oo3@users.noreply.github.com>
2025-04-27 17:29:59 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
           <div className="pl-4">
             <Link href="/">
               <Image
-                src="/logo.png" // Path to your logo in public folder
+                src="/Logo.png" // Path to your logo in public folder
                 alt="Company Logo" // Descriptive alt text
                 width={160} // Adjust to your logo's width
                 height={50} // Adjust to your logo's height
@@ -114,7 +114,7 @@ export default function Home() {
     <div className="max-w-2xl pl-8 md:pl-16 lg:pl-24 mt-16"> {/* Added mt-16 here */}
       <h1 className="text-5xl font-bold mb-2">Orazone Technologies</h1>
       <p className="text-xl mb-16">Diversified Business Solutions</p>
-      <div className="flex space-x-10">
+      <div className="flex space-x-18">
         <Link href="#" className="px-6 py-2 rounded-full bg-orange-500 hover:bg-orange-600 transition-colors">
           Contact Us
         </Link>
